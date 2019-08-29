d5905e2fa776f725aa8cfca7ac4fd325c1f4b8c1
Magnun A V F <magnunavf@gmail.com>
2019-08-28 21:42:49 -0300

    core: Add database_cleaner gem
    - To use in tests.

--- gifts-list-api/spec/spec_helper.rb
+++ gifts-list-api/spec/spec_helper.rb
@@ -38,4 +38,15 @@ RSpec.configure do |config|
   config.before(:suite) do
     FactoryBot.find_definitions
   end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
