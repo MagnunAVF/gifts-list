f0e1e56c2342520969df9aedd7664fa8d8130565
Magnun A V F <magnunavf@gmail.com>
2019-09-01 12:17:03 -0300

    refactor: Improve exception handler code
    - Reuse.

--- gifts-list-api/app/helpers/exception_handler.rb
+++ gifts-list-api/app/helpers/exception_handler.rb
@@ -3,49 +3,15 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::RecordNotFound do |e|
+    rescue_from ActiveRecord::RecordNotFound, ParentCategoryNotFoundError,
+                ClientNotFoundError, ProductNotFoundError, ListNotFoundError,
+                CategoryNotFoundError do |e|
       json_response({
         message: e.message,
       }, :not_found)
     end
 
-    rescue_from ParentCategoryNotFoundError do |e|
-      json_response({
-        message: e.message,
-      }, :not_found)
-    end
-
-    rescue_from ClientNotFoundError do |e|
-      json_response({
-        message: e.message,
-      }, :not_found)
-    end
-
-    rescue_from ProductNotFoundError do |e|
-      json_response({
-        message: e.message,
-      }, :not_found)
-    end
-
-    rescue_from ListNotFoundError do |e|
-      json_response({
-        message: e.message,
-      }, :not_found)
-    end
-
-    rescue_from CategoryNotFoundError do |e|
-      json_response({
-        message: e.message,
-      }, :not_found)
-    end
-
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({
-        message: e.message,
-      }, :unprocessable_entity)
-    end
-
-    rescue_from NoAttributesToUpdateError do |e|
+    rescue_from ActiveRecord::RecordInvalid, NoAttributesToUpdateError do |e|
       json_response({
         message: e.message,
       }, :unprocessable_entity)
